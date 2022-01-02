fdb22d1af4863ee5653d8412762a40035e94f9b3
SomeYurnero <uniquebizbox@gmail.com>
2022-01-02 18:29:27 +0200

    api practice

---
+++ src/services/MarvelService.js
@@ -0,0 +1,46 @@
+class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=a5aa21fb00687e162c5d43bb64481a67";
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        const charDescription = () => {
+            if (!char.description) {
+                return "Sorry, description is missing.";
+            } else if (char.description.length > 230) {
+                return char.description.slice(0, 231) + "...";
+            } else {
+                return char.description;
+            };
+        }
+
+        return {
+            name: char.name,
+            description: charDescription(),
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
